088e921d3fb20504bbc884aea2fb165b8e43435a
Jason Carrillo <jason.carrillo182@gmail.com>
2020-07-31 09:22:21 -0600

    completed cone selling exercise

--- js/while.js
+++ js/while.js
@@ -4,3 +4,18 @@ while(x <= 65536) {
     console.log(x)
     x *= 2
 }
+
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+do{
+    var cones = Math.floor(Math.random() * 5) + 1;
+    if (cones <= allCones) {
+        console.log(cones + " cones sold...");
+        allCones -= cones;
+    } else {
+        console.log("I cannot sell you " + cones + " cones I only have " + allCones);
+    }
+
+} while (allCones > 0);
+
+console.log("Yay! I sold them all!");
