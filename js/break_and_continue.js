fd10ac4f38b3d896b6def71f71fbd19940bd4f1f
Jason Carrillo <jason.carrillo182@gmail.com>
2020-07-31 08:56:44 -0600

    break_and_continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,17 +1,25 @@
 (function (){
 
 
-do { var enteredNumber = prompt("enter number")
-}while (enteredNumber > 49 || enteredNumber < 1 || enteredNumber % 2 != 1){
+// do { var enteredNumber = prompt("Enter an odd number between 1, and 50")
+// }while (enteredNumber > 49 || enteredNumber < 1 || enteredNumber % 2 != 1){
+// }
+
+while (true) {
+    var enteredNumber = parseInt(prompt("Enter an odd number 1-50"));
+    if (enteredNumber % 2 !== 0 && enteredNumber > 0 && enteredNumber <= 50)
+        break;
+    else continue;
 }
 
+console.log("Number to skip is: " + enteredNumber )
 for (var x = 1; x <= 49; x+= 2) {
     if (x == enteredNumber){
-        console.log("skipping " + enteredNumber)
+        console.log("Yikes! Skipping number: " + enteredNumber)
         continue;
     }
 
-    console.log("odd number" + x);
+    console.log("Here is an odd number: " + x);
 }
 
 })();
