3197643e0f17fa615e3b16df21469774789a6e85
Jason Carrillo <jason.carrillo182@gmail.com>
2020-08-31 15:57:09 -0600

    added all 3 restraunts to the map page

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -30,6 +30,42 @@ geocode("el paso, tx, 79938", mapBoxTokenAPI)
 
             });
 
+        geocode("9627 Sims Dr a, El Paso, TX 79925", mapBoxTokenAPI)
+            .then(function(result) {
+                console.log(result)
+
+                var marker = new mapboxgl.Marker()
+                    .setLngLat(result)
+                    .addTo(map);
+
+                var popupBam = new mapboxgl.Popup(marker)
+                    .setLngLat(result)
+                    .setHTML("<p>Bamboo Kitchen</p>")
+                    .addTo(map)
+
+                marker.setPopup(popupBam)
+
+
+            });
+
+        geocode("1360 George Dieter Dr, El Paso, TX 79936", mapBoxTokenAPI)
+            .then(function(result) {
+                console.log(result)
+
+                var marker = new mapboxgl.Marker()
+                    .setLngLat(result)
+                    .addTo(map);
+
+                var popupFam = new mapboxgl.Popup(marker)
+                    .setLngLat(result)
+                    .setHTML("<p>Bamboo Kitchen</p>")
+                    .addTo(map)
+
+                marker.setPopup(popupFam)
+
+
+            });
+
 
     });
 
