6078cec4e81b79531248b7d8673ee5ef38e88874
Jason Carrillo <jason.carrillo182@gmail.com>
2020-09-14 11:06:46 -0600

    added ajax files

---
+++ Mini Exercise.js
@@ -0,0 +1,28 @@
+{
+
+    "name": "bash",
+    "Number_of_students": 2,
+    "location": "Dallas, TX",
+    "students": ["student1", "student 2"],
+    "studentsArray": [
+    {
+        "name": {
+            "firstName": "Stacy",
+            "lastName": "R"
+        },
+        "DOB": "1990-01-24"
+        "Scholarship": true,
+        "Capstone": null,
+    },
+    {
+        "name": {
+            "firstName": "Juan",
+            "lastName": "M"
+        },
+        "DOB": "1992-05-13",
+        "Scholarship": false,
+        "Capstone": null,
+    },
+]
+
+}
