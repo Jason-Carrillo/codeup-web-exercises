5f4bf083e77733be564c217c1869a5005a4af988
Jason Carrillo <jason.carrillo182@gmail.com>
2020-09-16 10:50:11 -0600

    page updates depending on the city that is typed into the search bar

--- js/weather-map-box.js
+++ js/weather-map-box.js
@@ -1,34 +1,42 @@
-mapboxgl.accessToken = mapBoxToken;
+// mapboxgl.accessToken = mapBoxToken;
+//
+//
+//
+// geocode("El Paso, TX", mapBoxTokenAPI)
+//     .then(function(result) {
+//
+//         var typedCitySearch = $("#box-city-search").val()
+//         console.log(result)
+//         var map = new mapboxgl.Map({
+//             container: 'map',
+//             style: 'mapbox://styles/mapbox/navigation-guidance-night-v4', // stylesheet location
+//             center: result, // starting position [lng, lat]
+//             zoom: 10 // starting zoom
+//         });
+//
+//         geocode(typedCitySearch, mapBoxTokenAPI)
+//             .then(function(result) {
+//                 console.log(result)
+//
+//                 var marker = new mapboxgl.Marker()
+//                     .setLngLat(result)
+//                     .addTo(map);
+//
+//                 var popupPho = new mapboxgl.Popup(marker)
+//                     .setLngLat(result)
+//                     .setHTML("<p>Pho Tre Bien</p>")
+//                 // .addTo(map)
+//
+//                 marker.setPopup(popupPho)
+//
+//
+//             });
+//
+//
+//     });
 
 
 
-geocode("el paso, tx, 79938", mapBoxTokenAPI)
-    .then(function(result) {
-        console.log(result)
-        var map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/navigation-guidance-night-v4', // stylesheet location
-            center: result, // starting position [lng, lat]
-            zoom: 10 // starting zoom
-        });
-
-        geocode("12302 montana ave, el paso, tx, 79938", mapBoxTokenAPI)
-            .then(function(result) {
-                console.log(result)
-
-                var marker = new mapboxgl.Marker()
-                    .setLngLat(result)
-                    .addTo(map);
-
-                var popupPho = new mapboxgl.Popup(marker)
-                    .setLngLat(result)
-                    .setHTML("<p>Pho Tre Bien</p>")
-                // .addTo(map)
-
-                marker.setPopup(popupPho)
-
-
-            });
 
 
 
