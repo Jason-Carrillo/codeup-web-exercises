e616abac228606f13e8556ee91596a26245a9f34
Jason Carrillo <jason.carrillo182@gmail.com>
2020-09-28 14:45:41 -0600

    ran function after  assigning int

--- codeup-java-exercises/src/HelloWorld.java
+++ codeup-java-exercises/src/HelloWorld.java
@@ -2,5 +2,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
         System.out.print(" World!");
+
+        int myFavoriteNumber = 7;
+
+        System.out.println(myFavoriteNumber);
     }
 }
